ee2f7e84d140da9223a9901a7261dd509d8be348
Kamal <dragkamal71@gmail.com>
2025-01-14 17:11:29 +0530

    changes

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -1,4 +1,5 @@
 // const backend = 'http://localhost:5000/'
-const backend = 'https://chatt-app-kohl.vercel.app/'
+// const backend = 'https://chatt-app-kohl.vercel.app/'
+const backend ='https://chatt-app.onrender.com/'
 
 module.exports =   {backend};
